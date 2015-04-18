1227b304ded3c7daa9cb4de217a98aced22a78a7
Horopter <santoshdesai12@hotmail.com>
2015-04-18 20:01:22 +0530

    Version 1.3 update.

--- graph.h
+++ graph.h
@@ -1,7 +1,14 @@
-#include<GL/freeglut.h>
-#include<GL/freeglut_std.h>
-#include<GL/gl.h>
-#include<GL/glu.h>
-#include<GL/glut.h>
-#include<GL/glx.h>
+#if defined(__gnu_linux__)||defined(__linux__)
+	#include<GL/freeglut.h>
+	#include<GL/freeglut_std.h>
+	#include<GL/gl.h>
+	#include<GL/glu.h>
+	#include<GL/glut.h>
+	#include<GL/glx.h>
+#endif
+#if defined(_WIN32)||defined(__WINDOWS__)||defined(_WIN64)
+	#include<GL/gl.h>
+	#include<GL/glu.h>
+	#include<GL/glut.h>
+#endif
 
